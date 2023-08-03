9c9dbd4a68c10c1e65cc9a74260b89efac33931f
Toan Trinh <toan.trinh@ptnglobalcorp.com>
2023-08-03 16:05:50 +0700

    update variable

--- variables.tf
+++ variables.tf
@@ -1,12 +1,42 @@
-output "s3_bucket_public_arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value = try(aws_s3_bucket.public[0].arn, "")
+variable "environment" {
+  type = string
 }
 
-output "s3_bucket_private_arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value = try(aws_s3_bucket.private[0].arn, "")
+variable "region" {
+  type = string
 }
 
-                  
+variable "region_code" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+  default = "ptn"
+}
+
+variable "project" {
+  type = string
+}
+
+variable "create_private_bucket" {
+  description = "Create private bucket"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the bucket."
+  type        = map(string)
+  default     = {}
+}
+
+variable "force_destroy" {
+  type = bool
+  default = false
+}
+
+
+
+
 
